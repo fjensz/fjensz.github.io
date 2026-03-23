f2cda53ad299124ae689a0cfb70e0a9da0db7223
mpadge <mark.padgham@email.com>
2026-03-23 21:31:10 +0100

    New Site Build (Mon 23 Mar 21:31:10 CET 2026)

--- script.sh
+++ script.sh
@@ -27,11 +27,11 @@ rm -r dist
 sed -i '1d' index.html
 
 echo -e "\033[0;32mUpdating git...\033[0m"
-git add index.html _headers styleguide.html publications/* assets/*
+git add index.html _headers styleguide.html publications/ assets/
 git add -u
 git st
 git commit -am "New Site Build (`date`)"
-git push origin main
+git push origin gh-pages
 
 echo -e "\033[0;32mChange back to source branch...\033[0m"
 git checkout source
